8ce73e913ceb2e7f48501ee5a6d50233a018355c
pvpfoundation <admin@pvpfoundation.com>
2026-03-02 21:50:18 +0530

    Update images (#4)
    * Update homepage layout
    
    * First draft
    
    * second draft
    
    * Add images

--- layout.js
+++ layout.js
@@ -1,5 +1,19 @@
-fetch("header.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("layout-header").innerHTML = html;
-  });
+document.addEventListener("DOMContentLoaded", function () {
+
+  fetch("header.html")
+    .then(res => {
+      if (!res.ok) throw new Error("Header not found");
+      return res.text();
+    })
+    .then(html => {
+      document.getElementById("layout-header").innerHTML = html;
+
+      if (typeof initNav === "function") {
+        initNav();
+      }
+    })
+    .catch(err => {
+      console.error("Header failed to load:", err);
+    });
+
+});

7d61a0788f31ea60da5b69d2f0b426e62d7d35b4
Anand <admin@pvpfoundation.com>
2026-03-03 16:10:17 +0530

    Fix images JS file

--- netlify/functions/images.js
+++ netlify/functions/images.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async function () {
-  const directoryPath = path.join(process.cwd(), "images");
-
   try {
+    const directoryPath = path.join(__dirname, "../../images");
+
     const files = fs.readdirSync(directoryPath)
       .filter(file => /\.(jpg|jpeg|png|webp)$/i.test(file));
 
@@ -12,10 +12,11 @@ exports.handler = async function () {
       statusCode: 200,
       body: JSON.stringify(files),
     };
+
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Unable to scan directory" }),
+      body: JSON.stringify({ error: "Unable to scan directory", details: err.message }),
     };
   }
 };
